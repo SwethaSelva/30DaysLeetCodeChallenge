b8f3e52e6f6e9a1bf912e2e36cddf5f423d505e6
S.Swetha <swethalakshmi64@gmail.com>
2022-08-05 23:55:23 +0530

    day 3

---
+++ remuvDuplicateSortArr.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+  let prev = nums[0];
+  let ptr = 1;
+  for (let i = 1; i < nums.length; i++) {
+      if (prev !== nums[i]) {
+          nums[ptr] = nums[i];
+          prev = nums[i]
+          ptr++;
+      }
+  }
+  return ptr;
+};
+
+// Leet code
+var removeDuplicates = function(nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[j] != nums[i]) {
+      j++;
+      nums[j] = nums[i];
+    }
+  }
+  return j + 1;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+
+
+"snvrcpaywg"
+"bpzqiabvfoycdesgjqzijqwdgvsetpbxltggvngvitbddfoatgxjgweahffmlfbnrqjxntqoxshzrvwsvchxhlwjablxfxebdlyyogepeoevrozsmuhxtzwuanrzjqsjpfcnoxzmkfskvttwbbijgnvmlvrhusyncngiagjveozhyeyaqgqqwawbckimryslymostqrgbfaqfnzyczijisjwtuvlufvaugeebmkwxrouvzhfujlhwbwhfgkanjkbvoqmtyabcbwrkpuognuyhjzvxktsouwrfpsqrpcrrzqyuvxawkijnoznlxouhuwgwkfuihnjtcklvpgrzzblybnoznhsqyvyjoyzxkdbznhidzuwqbsjjtcsixxfjhvdvsmhtgepexrhddybqncmsomhxjgiruedpsrsasnosxavmomyxhdkeziwapjkscapaerzxstvfygmqdejitsefuiubyzclqtsxjhadtsybhmyrbjtqaxjkmhzwmqndlxuxvsuudnxqpaveddpkqbeziiziywtzazgpinmnzspruzvhelzseioyrdjsqjshmczkvrkqylbmxsrbaxcvisqkfcosonnfbveucnkcinfazivtfbcarkmpkyggiondhpzrcwefsozefgpftoikjwqgyxbyxucjsrmhecuyybqkyipbpxuncpobfmwjlwjluricyzjvrlqtqpdhqfyoezqshascjpqamewpharnsmlfzoorxtpuwpnbkgpzkganupisydhbzfrzhzvanspwrnvcaypomxiwzenytaodqmgeayivdwiifgkpaicihqhgmvjmromkclqrfgpkeoirinccsxvmylxcgqdktusjpxwmqxasmwloxjyjxqqkghqipztfuygkqpkywtywdoxdeljwttcwanolzdacvsnxqbdmbswhzzfzxogpcsxxasgzzqqekabfpnjkkltdrufgukbporvabcbhslxvpdiogmrrtvncqmpykeqaklzwylbvbxlrnwrxosmgdbpilqhbdlbduygiukhflnwoshsmuauiqrwvrpiqekhpziipcphktcwppsvxtrpxiyubxymkzvdnvqazvuhjpayuorhfeknrkwjjzwwycorfjilaabtfgyqpqzxlfrlufdzxujanmyhmalqlkbhnydmbftgcxqitabxhiehmuwauinaukktcubfdhchzmouqfvhzijfitfjbnlnwkupdvqniylsufidhmmszwcipxkktmnruthlxtdnekdjcoxhhyiesazkwqgfusfaevniordhhazhslskhzepyzkpwstea"
