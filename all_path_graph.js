0975c2e5a6d07ee5c7f814e376178bfde2174d4b
S.Swetha <swethalakshmi64@gmail.com>
2022-08-07 21:15:30 +0530

    day5 - Graph DFS

---
+++ all_path_graph.js
@@ -0,0 +1,21 @@
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+  let destNode = graph.length - 1;
+  let result = [];
+  let visitedNode = {};
+  (function findDest (curNode = 0, curPath = []) {
+    if (curNode === destNode) return result.push([...curPath, curNode]);
+    for (let i = 0; i < graph[curNode].length; i++) {
+      let childNode = graph[curNode][i];
+      findDest(childNode, [...curPath, curNode], result);
+    }
+  })();
+  return result;
+};
+
+console.log(allPathsSourceTarget([[4,3,1],[3,2,4],[3],[4],[]]));
+console.log(allPathsSourceTarget([[]]));
