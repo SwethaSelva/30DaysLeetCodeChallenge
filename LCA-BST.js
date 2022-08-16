7b4fb1c1f6604a79632478e4bcf8422953bf265c
S.Swetha <swethalakshmi64@gmail.com>
2022-08-16 08:21:29 +0530

    marking easy problem

--- LCA-BST.js
+++ LCA-BST.js
@@ -1,6 +1,6 @@
 /**
  * Definition for a binary tree node.
- * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/ - easy
  */
 function TreeNode(val) {
   this.val = val;
